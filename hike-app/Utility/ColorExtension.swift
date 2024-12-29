5cc941d569ccd9957d9fd13056b52596c93ac044
shouta0715 <Myheroes0715@gmail.com>
2024-12-29 19:23:55 +0900

    add: custom color

---
+++ hike-app/Utility/ColorExtension.swift
@@ -0,0 +1,22 @@
+//
+//  ColorExtension.swift
+//  hike-app
+//
+//  Created by k21047kk on 2024/12/29.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customGreenLight = Color("ColorGreenLight")
+    static let customGreenMedium = Color("ColorGreenMedium")
+    static let customGreenDark = Color("ColorGreenDark")
+    
+    static let customGrayLight = Color("ColorGrayLight")
+    static let customGrayMedium = Color("ColorGrayMedium")
+    
+    static let customIndigoMedium = Color("ColorIndigoMedium")
+    
+    static let customSalmonLight = Color("ColorSalmonLight")
+}
