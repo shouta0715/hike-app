c49d9c17387567f0baeae13d12fa8914985f4bfc
shouta0715 <Myheroes0715@gmail.com>
2024-12-29 19:24:22 +0900

    add: card view

--- hike-app/Components/CardView.swift
+++ hike-app/Components/CardView.swift
@@ -10,22 +10,27 @@ import SwiftUI
 struct CardView: View {
     var body: some View {
         ZStack {
-            Circle()
-                .fill(
-                    LinearGradient(
-                        colors: [
-                            Color("ColorIndigoMedium"),
-                            Color("ColorSalmonLight")
-                        ],
-                        startPoint: .topLeading,
-                        endPoint: .bottomTrailing
-                    )
-                ).frame(width: 256,height: 256)
+            CustomBackgroundView()
+            
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            colors: [
+                                Color("ColorIndigoMedium"),
+                                Color("ColorSalmonLight")
+                            ],
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing
+                        )
+                    ).frame(width: 256,height: 256)
                 
-            Image("image-1")
-                .resizable()
-                .scaledToFit()
+                Image("image-1")
+                    .resizable()
+                    .scaledToFit()
+            }
         }
+        .frame(width: 320,height: 570)
     }
 }
 
