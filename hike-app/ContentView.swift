b27fa9ad38a6397e2fe80e4fd0cef376064a0565
shouta0715 <Myheroes0715@gmail.com>
2024-12-29 15:07:17 +0900

    Initial Commit

---
+++ hike-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hike-app
+//
+//  Created by k21047kk on 2024/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hike-app/hike_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  hike_appApp.swift
+//  hike-app
+//
+//  Created by k21047kk on 2024/12/29.
+//
+
+import SwiftUI
+
+@main
+struct hike_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
