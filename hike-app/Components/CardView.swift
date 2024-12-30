d63b8c53a85a70867496e5989e7291d14e35009d
shouta0715 <Myheroes0715@gmail.com>
2024-12-30 22:28:37 +0900

    add: header text

--- hike-app/Components/CardView.swift
+++ hike-app/Components/CardView.swift
@@ -12,22 +12,60 @@ struct CardView: View {
         ZStack {
             CustomBackgroundView()
             
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(
-                            colors: [
-                                Color("ColorIndigoMedium"),
-                                Color("ColorSalmonLight")
-                            ],
-                            startPoint: .topLeading,
-                            endPoint: .bottomTrailing
-                        )
-                    ).frame(width: 256,height: 256)
+            VStack {
+                // MARK: - Header
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [.customGrayLight,.customGrayMedium],
+                                    startPoint: .top,
+                                    endPoint: .bottom
+                                )
+                            )
+                        
+                        Button {
+                            // ACTION: Show a Sheet
+                            print("The button was tapped")
+                        }label: {
+                            Text("Button")
+                        }
+                    }
+                    
+                    Text(
+                        "Fun and enjoyable outdoor activity for friends and familes"
+                    )
+                    .multilineTextAlignment(.leading)
+                    .italic()
+                    .foregroundColor(.customGrayMedium)
+                }
+                .padding(.horizontal,40)
                 
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+                // MARK: - Main Content
+                
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                colors: [
+                                    Color("ColorIndigoMedium"),
+                                    Color("ColorSalmonLight")
+                                ],
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        ).frame(width: 256,height: 256)
+                    
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                // MARK: - Main Footer
             }
         }
         .frame(width: 320,height: 570)
