1088367dd12bdfbfbea451318dc6258682eb7880
shouta0715 <Myheroes0715@gmail.com>
2024-12-31 14:08:53 +0900

    fix: motion animation

--- hike-app/Components/CustomCircleView.swift
+++ hike-app/Components/CustomCircleView.swift
@@ -12,25 +12,28 @@ struct CustomCircleView: View {
     
     
     var body: some View {
-        Circle()
-            .fill(
-                LinearGradient(
-                    colors: [
-                        .customIndigoMedium,
-                        .customSalmonLight
-                    ],
-                    startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
-                    endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(
+                        colors: [
+                            .customIndigoMedium,
+                            .customSalmonLight
+                        ],
+                        startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
+                        endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing
+                    )
                 )
-            )
-            .onAppear {
-                withAnimation(
-                    .linear(duration: 3.0).repeatForever(autoreverses: true)
-                ) {
-                    isAnimateGradient.toggle()
+                .onAppear {
+                    withAnimation(
+                        .linear(duration: 3.0).repeatForever(autoreverses: true)
+                    ) {
+                        isAnimateGradient.toggle()
+                    }
                 }
-            }
-            .frame(width: 256,height: 256)
+            MotionAnimationView()
+        }
+        .frame(width: 256,height: 256)
     }
 }
 
---
+++ hike-app/Components/MotionAnimationView.swift
@@ -0,0 +1,81 @@
+//
+//  MotionAnimationView.swift
+//  hike-app
+//
+//  Created by k21047kk on 2024/12/31.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    let frameSize: CGFloat = 256
+    
+    // MARK: - PROPERTIES
+    @State private var randomCircle:Int  = Int.random(in: 6...12)
+    @State private var isAnimating: Bool =  false
+    
+    // MARK: - FUNCTION
+    
+    // 1. RANDOM COORDINATE
+    func ramdomCoordinate() -> CGFloat {
+        return CGFloat.random(in: 0...frameSize)
+    }
+    // 2. RANDOM SIZE
+    func ramdomSize() -> CGFloat {
+        return CGFloat(Int.random(in: 4...80))
+    }
+    // 3. RANDOM SCALE
+    func ramdomScale() -> CGFloat {
+        return CGFloat(Double.random(in: 0.1...2.0))
+    }
+    // 4. RANDOM SPEED
+    func ramdomSpeed() -> Double {
+        return Double.random(in: 0.05...1.0)
+    }
+    // 5. RANDOM DELAY
+    func ramdomDelay() -> Double {
+        return Double.random(in: 0...2)
+    }
+    
+    var body: some View {
+        ZStack{
+            ForEach(0...randomCircle, id: \.self) { item in
+                Circle()
+                    .foregroundColor(.white)
+                    .opacity(0.25)
+                    .frame(
+                        width: ramdomSize(),
+                        height: ramdomSize()
+                    )
+                    .position(
+                        x: ramdomCoordinate(),
+                        y: ramdomCoordinate()
+                    )
+                    .scaleEffect(isAnimating ? ramdomScale() : 1)
+                    .onAppear(perform: {
+                        withAnimation(
+                            .interpolatingSpring(stiffness: 0.25, damping: 0.25)
+                            .repeatForever()
+                            .speed(ramdomSpeed())
+                            .delay(ramdomDelay())
+                        ){
+                            isAnimating = true
+                        }
+                    })
+                
+            }
+        } //: ZStack
+        .frame(width: frameSize, height: frameSize)
+        .mask(Circle())
+        .drawingGroup()
+    }
+}
+
+#Preview {
+    
+    MotionAnimationView().background(
+        Circle()
+        .fill(.teal)
+    )
+
+}
